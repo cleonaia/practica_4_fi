93751055dfeeb0a7bbdd7c022cd60dcc78b49bc3
Zhengli Sunzhu <1798600@uab.cat>
2025-12-01 09:29:21 +0100

    Create my_tr.c

---
+++ my_tr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int open(const char * cities_ES.tsv, int flags){
+    
+    for(int i = 0; i < len(open); i++) {
+        ssize_t read(int fd, void *buf, size_t 1);
+        if (read[i] == "\t") {}
+            printf(read[i]",");
+        elif (read[i] == ' ') {
+            return "-d";
+        }
+        elif (read[i] == read[i+1] && i<len(cities_ES.csv)){
+            return "-s";
+        }
+        else {
+            ssize_t write(int fd, void *buf, size_t 1);
+            printf(write[i]);
+        }
+        }
+    return 0;
+    }
+}
+    
