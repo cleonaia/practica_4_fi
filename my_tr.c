6bddff4c75484317680fc40d432357845b7e9a55
Leo <1707505@uab.cat>
2025-12-01 09:56:57 +0100

    Update my_tr.c

--- my_tr.c
+++ my_tr.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    int open(const char * cities_ES.tsv, int flags){
-    
-    for(int i = 0; i < len(open); i++) {
-        ssize_t read(int fd, void *buf, size_t 1);
-        if (read[i] == "\t") {}
-            printf(read[i]",");
-        elif (read[i] == ' ') {
-            return "-d";
-        }
-        elif (read[i] == read[i+1] && i<len(cities_ES.csv)){
-            return "-s";
-        }
-        else {
-            ssize_t write(int fd, void *buf, size_t 1);
-            printf(write[i]);
-        }
-        }
-    return 0;
+int main(int argc, char *argv[]) {
+    enum { MODE_SUBST, MODE_DELETE, MODE_SQUASH } mode = MODE_SUBST;
+    unsigned char prev = 0;
+    int have_prev = 0;
+
+    if (argc == 1) {
+        mode = MODE_SUBST;         // default: replace '\t' with ','
+    } else if (argc == 2 && strcmp(argv[1], "-d") == 0) {
+        mode = MODE_DELETE;        // delete spaces ' '
+    } else if (argc == 2 && strcmp(argv[1], "-s") == 0) {
+        mode = MODE_SQUASH;        // remove consecutive duplicates
+    } else {
+        fprintf(stderr,
+                "Ús:\n  %s            # substitueix \\t per ,\n  %s -d         # elimina espais\n  %s -s         # elimina repeticions consecutives\n",
+                argv[0], argv[0], argv[0]);
+        return 1;
     }
+
+    unsigned char buf[4096];
+    ssize_t n;
+
+    while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+        for (ssize_t i = 0; i < n; ++i) {
+            unsigned char c = buf[i];
+
+            if (mode == MODE_SUBST) {
+                if (c == '\t') c = ',';
+                if (write(STDOUT_FILENO, &c, 1) != 1) { perror("write"); return 1; }
+            } else if (mode == MODE_DELETE) {
+                if (c == ' ') continue;
+                if (write(STDOUT_FILENO, &c, 1) != 1) { perror("write"); return 1; }
+            } else { // MODE_SQUASH
+                if (have_prev && c == prev) continue;
+                if (write(STDOUT_FILENO, &c, 1) != 1) { perror("write"); return 1; }
+                prev = c;
+                have_prev = 1;
+            }
+        }
+    }
+    if (n < 0) { perror("read"); return 1; }
+    return 0;
 }
     
